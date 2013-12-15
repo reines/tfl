751592b81ff6f5ce0829b6919de4b2d231828f90
Jamie Furness <jfurness@yammer-inc.com>
2013-12-15 22:40:46 +0000

    Removing lots of the old map stuff

--- tfl-service/src/main/resources/assets/js/map.js
+++ tfl-service/src/main/resources/assets/js/map.js
@@ -14,7 +14,7 @@ $(document).ready(function() {
 
 function highlightRoute(map, percent, line, stops) {
 	map.segments(line, stops)
-		.attr("stroke-width", (percent*3)+5 + "px")
+		.attr("stroke-width", (percent * 3) + 5)
 		.attr("opacity", "1");
 	
 	highlightStation(map, percent, line, stops[0]);
@@ -22,95 +22,98 @@ function highlightRoute(map, percent, line, stops) {
 }
 
 function highlightStation(map, percent, line, stationName) {
-	// Add Text
-	//console.log(stationNames);
-//	var stationLabel = stationNames[getJustStationId(stationName)];
-//	console.log(stationLabel);
-//	map.svg({
-//        onLoad: function(s) {
-//        	console.log(s);
-//        	s.text(stationLabel.x, stationLabel.y, stationLabel.label, { fill: red});
-//        }
-//        });
-
-	// Add Circle
 	map.station(line, stationName)
-		.attr("r", (percent*5)+5 + "px")
+		.attr("r", (percent * 5) + 5)
 		.attr("opacity", "1");
+
+    map.label(stationName)
+        .attr("opacity", "1");
 }
 
 (function($) {
     $.fn.drawLondonUnderground = function(lines, stations, connections, options) {
         options = $.extend({}, {
             lineWidth: 5,
-            lineOpacity: 0.1,
+            lineOpacity: 0.3,
+
+            stationSize: 20,
             stationOpacity: 0,
-            stationSize: 5,
+
             textSize: 12.5,
-            textColour: "#1c3f94"
+            textColour: "#1c3f94",
+            textOpacity: 0
         }, options);
 
         return this.each(function() {
-            var $element = $(this);
+            $(this).svg(function(map) {
+                // For each set of connections belonging to a line
+                _.each(connections, function(lineConnections, lineId) {
+                    var line = lines[lineId];
+                    var $svgLineGroup = $(map.group())
+                        .attr("id", line.getId());
 
-            $element.svg({
-                onLoad: function(map) {
-                    // For each set of connections belonging to a line
-                    _.each(connections, function(lineConnections, lineId) {
-                        var line = lines[lineId];
-                        var svgLineGroup = map.group({ id: line.getId() });
+                    // For each connection in this line
+                    _.each(lineConnections, function(connection) {
+                        var line = connection.stationA.line;
+                        var stationA = connection.stationA;
+                        var stationB = connection.stationB;
 
-                        // For each connection in this line
-                        _.each(lineConnections, function(connection) {
-                            var line = connection.stationA.line;
-                            var stationA = connection.stationA;
-                            var stationB = connection.stationB;
+                        var $svgConnection;
+                        if (connection.joinSvg === undefined) {
+                            $svgConnection = $(map.line($svgLineGroup, stationA.x, stationA.y, stationB.x, stationB.y));
+                        }
+                        else {
+                            $svgConnection = $(map.path($svgLineGroup, "M " + stationA.getCoords() + " L " + connection.joinSvg + " L " + stationB.getCoords()));
+                        }
 
-                            var svgConnection;
-                            if (connection.joinSvg === undefined) {
-                                svgConnection = map.line(svgLineGroup, stationA.x, stationA.y, stationB.x, stationB.y);
-                            }
-                            else {
-                                svgConnection = map.path(svgLineGroup, "M " + stationA.getCoords() + " L " + connection.joinSvg + " L " + stationB.getCoords());
-                            }
-
-                            $(svgConnection)
-                                .addClass("segment")
-                                .addClass("line-" + stationA.line.getId())
-                                .addClass("station-" + stationA.getId())
-                                .addClass("station-" + stationB.getId())
-                                .attr("stroke", line.colour)
-                                .attr("opacity", options.lineOpacity)
-                                .attr("stroke-width", options.lineWidth);
-                        });
+                        $svgConnection
+                            .addClass("segment")
+                            .addClass("line-" + stationA.line.getId())
+                            .addClass("station-" + stationA.getId())
+                            .addClass("station-" + stationB.getId())
+                            .attr("fill", "none")
+                            .attr("stroke", line.colour)
+                            .attr("opacity", options.lineOpacity)
+                            .attr("stroke-width", options.lineWidth);
                     });
+                });
 
-                    // For each station draw a dot
+                // For each station draw a dot
                     _.each(stations, function(station) {
-                        var svgLineGroup = $("#" + station.line.getId())[0];
+                        var $svgLineGroup = $("#" + station.line.getId());
 
-                        var svgStation = map.circle(svgLineGroup, station.x, station.y, options.stationSize);
-                        $(svgStation)
+                        $(map.circle($svgLineGroup, station.x, station.y, options.stationSize))
                             .addClass("station")
                             .addClass("line-" + station.line.getId())
                             .addClass("station-" + station.getId())
                             .attr("fill", station.line.colour)
                             .attr("opacity", options.stationOpacity);
-
-                        var svgLabel = map.text(svgLineGroup, station.x + 10, station.y - 5, station.getName());
-                        $(svgLabel)
-                            .addClass("label")
-                            .attr("font", "Myriad")
-                            .attr("font-size", options.textSize)
-                            .attr("fill", options.textColour);
                     });
-                }
+
+                var $svgLabelGroup = $(map.group())
+                    .attr("id", "labels");
+
+                // For each label write the text
+                _.each(stationLabels, function(label) {
+                    $(map.text($svgLabelGroup, label.getX(), label.getY(), label.text))
+                        .addClass("label")
+                        .addClass("label-" + label.getId())
+                        .attr("text-anchor", label.getAnchor())
+                        .attr("font", "Myriad")
+                        .attr("font-size", options.textSize)
+                        .attr("fill", options.textColour)
+                        .attr("opacity", options.textOpacity);
+                });
             });
         });
     };
 
     $.fn.station = function(line, name) {
         return this.find(".station.station-" + getStationId(line, name));
+    };
+
+    $.fn.label = function(name) {
+        return this.find(".label.label-" + getJustStationId(name));
     };
 
     $.fn.route = function(name) {
@@ -124,13 +127,13 @@ function highlightStation(map, percent, line, stationName) {
     };
 
     $.fn.segments = function(line, stationNames) {
-    	var domElement = this;
+    	var $element = $(this);
     	var segments = [];
 
         _.each(stationNames, function(stationAName, index) {
     		if (index + 1 == stationNames.length) return;
 
-            segments.push(domElement.segment(line, stationAName, stationNames[index+1]));
+            segments.push($element.segment(line, stationAName, stationNames[index + 1]));
     	});
 
     	return $(segments);
--- tfl-service/src/main/resources/assets/js/tubeMapMaker.js
+++ tfl-service/src/main/resources/assets/js/tubeMapMaker.js
@@ -1,5 +1,386 @@
+var LINE_BAKERLOO = "Bakerloo";
+var LINE_CENTRAL = "Central";
+var LINE_CIRCLE = "Circle";
+var LINE_DISTRICT = "District";
+var LINE_HAMMERSMITH = "Hammersmith & City";
+var LINE_JUBILEE = "Jubilee";
+var LINE_METROPOLITAN = "Metropolitan";
+var LINE_NOTHERN = "Nothern";
+var LINE_PICADILLY = "Piccadilly";
+var LINE_VICTORIA = "Victoria";
+var LINE_WATERLOO = "Waterloo & City";
+var LINE_DLR = "DLR";
+var LINE_OVERGROUND = "London Overground";
+
+var ACTON_TOWN = "Acton Town";
+var ACTON_CENTRAL = "Acton Central";
+var ALDGATE = "Aldgate";
+var ALDGATE_EAST = "Aldgate East";
+var ALL_SAINTS = "All Saints";
+var ALPERTON = "Alperton";
+var AMERSHAM = "Amersham";
+var ANGEL = "Angel";
+var ARCHWAY = "Archway";
+var ARNOS_GROVE = "Arnos Grove";
+var ARSENAL = "Arsenal";
+var BAKER_STREET = "Baker Street";
+var BALHAM = "Balham";
+var BANK = "Bank";
+var BARBICAN = "Barbican";
+var BARKING = "Barking";
+var BARKINGSIDE = "Barkingside";
+var BARONS_COURT = "Barons Court";
+var BAYSWATER = "Bayswater";
+var BECONTREE = "Becontree";
+var BELSIZE_PARK = "Belsize Park";
+var BERMONDSEY = "Bermondsey";
+var BETHNAL_GREEN = "Bethnal Green";
+var BLACKFRIARS = "Blackfriars";
+var BLACKHORSE_ROAD = "Blackhorse Road";
+var BOND_STREET = "Bond Street";
+var BOROUGH = "Borough";
+var BOSTON_MANOR = "Boston Manor";
+var BOUNDS_GREEN = "Bounds Green";
+var BOW_CHURCH = "Cow Church";
+var BOW_ROAD = "Bow Road";
+var BRENT_CROSS = "Brent Cross";
+var BRIXTON = "Brixton";
+var BROMLEY_BY_BOW = "Bromley-by-Bow";
+var BRONDESBURY = "Brondesbury";
+var BRONDESBURY_PARK = "Brondesbury Park";
+var BUCKHURST_HILL = "Buckhurst Hill";
+var BURNT_OAK = "Burnt Oak";
+var CALEDONIAN_ROAD = "Caledonian Road";
+var CAMDEN_ROAD = "Camden Road";
+var CAMDEN_TOWN_1 = "Camden Town (1)";
+var CAMDEN_TOWN_2 = "Camden Town (2)";
+var CANADA_WATER = "Canada Water";
+var CANARY_WHARF = "Canary Wharf";
+var CANNING_TOWN = "Canning Town";
+var CANNON_STREET = "Cannon Street";
+var CANNONS_PARK = "Cannons Park";
+var CANONBURY = "Canonbury";
+var CHALFONT_AND_LATIMER = "Chalfont & Latimer";
+var CHALK_FARM = "Chalk Farm";
+var CHANCERY_LANE = "Chancery Lane";
+var CHARING_CROSS = "Charing Cross";
+var CHESHAM = "Chesham";
+var CHIGWELL = "Chigwell";
+var CHISWICK_PARK = "Chiswick Park";
+var CHORLEYWOOD = "Chorleywood";
+var CLAPHAM_COMMON = "Clapham Common";
+var CLAPHAM_NORTH = "Clapham North";
+var CLAPHAM_SOUTH = "Clapham South";
+var COCKFOSTERS = "Cockfosters";
+var COLINDALE = "Colindale";
+var COLLIERS_WOOD = "Colliers Wood";
+var COVENT_GARDEN = "Covent Garden";
+var CROSSHARBOUR = "Crossharbour";
+var CROXLEY = "Croxley";
+var DAGENHAM_EAST = "Dagenham East";
+var DAGENHAM_HEATHWAY = "Dagenham Heathway";
+var DALSTON_KINGSLAND = "Dalston Kingsland";
+var DEBDEN = "Debden";
+var DEVONS_ROAD = "Devons Road";
+var DOLLIS_HILL = "Dollis Hill";
+var EALING_BROADWAY = "Ealing Broadway";
+var EALING_COMMON = "Ealing Common";
+var EARLS_COURT = "Earl's Court";
+var EAST_ACTON = "East Acton";
+var EAST_FINCHLEY = "East Finchley";
+var EAST_HAM = "East Ham";
+var EAST_PUTNEY = "East Putney";
+var EASTCOTE = "Eastcote";
+var EDGWARE = "Edgware";
+var EDGWARE_ROAD = "Edgware Road";
+var EDGWARE_ROAD_1 = "Edgware Road (1)";
+var EDGWARE_ROAD_2 = "Edgware Road (2)";
+var ELEPHANT_AND_CASTLE = "Elephant & Castle";
+var ELM_PARK = "Elm Park";
+var EMBANKMENT = "Embankment";
+var EPPING = "Epping";
+var EUSTON_1 = "Euston (1)";
+var EUSTON_2 = "Euston (2)";
+var EUSTON_SQUARE = "Euston Square";
+var FAIRLOP = "Fairlop";
+var FARRINGDON = "Farringdon";
+var FINCHLEY_CENTRAL = "Finchley Central";
+var FINCHLEY_ROAD = "Finchley Road";
+var FINSBURY_PARK = "Finsbury Park";
+var FULHAM_BROADWAY = "Fulham Broadway";
+var GANTS_HILL = "Gants Hill";
+var GLOUCESTER_ROAD = "Gloucester Road";
+var GOLDERS_GREEN = "Golders Green";
+var GOLDHAWK_ROAD = "Goldhawk Road";
+var GOODGE_STREET = "Goodge Street";
+var GOSPEL_OAK_1 = "Gospel Oak (1)";
+var GOSPEL_OAK_2 = "Gospel Oak (2)";
+var GRANGE_HILL = "Grange Hill";
+var GREAT_PORTLAND_STREET = "Great Portland Street";
+var GREENFORD = "Greenford";
+var GUNNERSBURY = "Gunnersbury";
+var GREEN_PARK = "Green Park";
+var HACKNEY_CENTRAL = "Hackney Central";
+var HACKNEY_WICK = "Hackney Wick";
+var HAINAULT = "Hainault";
+var HAMMERSMITH = "Hammersmith";
+var HAMPSTEAD = "Hampstead";
+var HAMPSTEAD_HEATH = "Hampstead Heath";
+var HANGER_LANE = "Hanger Lane";
+var HARLESDEN = "Harlesden";
+var HARROW_AND_WEALDSTONE = "Harrow & Wealdstone";
+var HARROW_ON_THE_HILL = "Harrow-on-the-Hill";
+var HATTON_CROSS = "Hatton Cross";
+var HEATHROW_TERMINALS_123 = "Heathrow Terminals 1-2-3";
+var HEATHROW_TERMINAL_4 = "Heathrow Terminal 4";
+var HEATHROW_TERMINAL_5 = "Heathrow Terminal 5";
+var HENDON_CENTRAL = "Hendon Central";
+var HERON_QUAYS = "Heron Quays";
+var HIGH_BARNET = "High Barnet";
+var HIGH_STREET_KENSINGTON = "High Street Kensington";
+var HIGHBURY_AND_ISLINGTON = "Highbury & Islington";
+var HIGHGATE = "Highgate";
+var HILLINGDON = "Hillingdon";
+var HOLBORN = "Holborn";
+var HOLLAND_PARK = "Holland Park";
+var HOLLOWAY_ROAD = "Holloway Road";
+var HOMERTON = "Homerton";
+var HORNCHURCH = "Hornchurch";
+var HOUNSLOW_CENTRAL = "Hounslow Central";
+var HOUNSLOW_EAST = "Hounslow East";
+var HOUNSLOW_WEST = "Hounslow West";
+var HYDE_PARK_CORNER = "Hyde Park Corner";
+var ICKENHAM = "Ickenham";
+var KENNINGTON = "Kennington";
+var KENSAL_GREEN = "Kensal Green";
+var KENSAL_RISE = "Kensal Rise";
+var KENSINGTON = "Kensington";
+var KENTISH_TOWN = "Kentish Town";
+var KENTISH_TOWN_WEST = "Kentish Town West";
+var KENTON = "Kenton";
+var KEW_GARDENS = "Kew Gardens";
+var KILBURN = "Kilburn";
+var KILBURN_PARK = "Kilburn Park";
+var KINGS_CROSS = "King's Cross St. Pancras";
+var KINGSBURY = "Kingsbury";
+var KNIGHTSBRIDGE = "Knightsbridge";
+var LADBROKE_GROVE = "Ladbroke Grove";
+var LAMBETH_NORTH = "Lambeth North";
+var LANCASTER_GATE = "Lancaster Gate";
+var LANGDON_PARK = "Langdon Park";
+var LATIMER_ROAD = "Latimer Road";
+var LEICESTER_SQUARE = "Leicester Square";
+var LEYTON = "Leyton";
+var LEYTONSTONE = "Leytonstone";
+var LIVERPOOL_STREET = "Liverpool Street";
+var LONDON_BRIDGE = "London Bridge";
+var LOUGHTON = "Loughton";
+var MAIDA_VALE = "Maida Vale";
+var MANOR_HOUSE = "Manor House";
+var MANSION_HOUSE = "Mansion House";
+var MARBLE_ARCH = "Marble Arch";
+var MARYLEBONE = "Marylebone";
+var MILE_END = "Mile End";
+var MILL_HILL_EAST = "Mill Hill East";
+var MONUMENT = "Monument";
+var MOOR_PARK = "Moor Park";
+var MOORGATE = "Moorgate";
+var MORDEN = "Morden";
+var MORNINGTON_CRESCENT = "Mornington Crescent";
+var NEASDEN = "Neasden";
+var NEW_CROSS = "New Cross";
+var NEW_CROSS_GATE = "New Cross Gate";
+var NEWBURY_PARK = "Newbury Park";
+var NORTH_ACTON = "North Acton";
+var NORTH_EALING = "North Ealing";
+var NORTH_GREENWICH = "North Greenwich";
+var NORTH_HARROW = "North Harrow";
+var NORTH_WEMBLEY = "North Wembley";
+var NORTHFIELDS = "Northfields";
+var NORTHOLT = "Northolt";
+var NORTHWICK_PARK = "Northwick Park";
+var NORTHWOOD = "Northwood";
+var NORTHWOOD_HILLS = "Northwood Hills";
+var NOTTING_HILL_GATE = "Notting Hill Gate";
+var OAKWOOD = "Oakwood";
+var OLD_STREET = "Old Street";
+var OSTERLEY = "Osterley";
+var OVAL = "Oval";
+var OXFORD_CIRCUS = "Oxford Circus";
+var PADDINGTON_1 = "Paddington (1)";
+var PADDINGTON_2 = "Paddington (2)";
+var PADDINGTON_3 = "Paddington (3)";
+var PARK_ROYAL = "Park Royal";
+var PARSONS_GREEN = "Parsons Green";
+var PERIVALE = "Perivale";
+var PICCADILLY_CIRCUS = "Piccadilly Circus";
+var PIMLICO = "Pimlico";
+var PINNER = "Pinner";
+var PLAISTOW = "Plaistow";
+var POPLAR = "Poplar";
+var PRESTON_ROAD = "Preston Road";
+var PUDDING_MILL_LANE = "Pudding Mill Lane";
+var PUTNEY_BRIDGE = "Putney Bridge";
+var QUEENS_PARK = "Queen's Park";
+var QUEENSBURY = "Queensbury";
+var QUEENSWAY = "Queensway";
+var RAVENSCOURT_PARK = "Ravenscourt Park";
+var RAYNERS_LANE = "Rayners Lane";
+var REDBRIDGE = "Redbridge";
+var REGENTS_PARK = "Regent's Park";
+var RICHMOND = "Richmond";
+var RICKMANSWORTH = "Rickmansworth";
+var RODING_VALLEY = "Roding Valley";
+var ROTHERHITHE = "Rotherhithe";
+var ROYAL_OAK = "Royal Oak";
+var RUISLIP = "Ruislip";
+var RUISLIP_GARDENS = "Ruislip Gardens";
+var RUISLIP_MANOR = "Ruislip Manor";
+var RUSSELL_SQUARE = "Russell Square";
+var SEVEN_SISTERS = "Seven Sisters";
+var SHADWELL = "Shadwell";
+var SHEPHERDS_BUSH = "Shepherd's Bush";
+var SHEPHERDS_BUSH_MARKET = "Shepherd's Bush Market";
+var SLOANE_SQUARE = "Sloane Square";
+var SNARESBROOK = "Snaresbrook";
+var SOUTH_ACTON = "South Acton";
+var SOUTH_EALING = "South Ealing";
+var SOUTH_HARROW = "South Harrow";
+var SOUTH_KENSINGTON = "South Kensington";
+var SOUTH_KENTON = "South Kenton";
+var SOUTH_RUISLIP = "South Ruislip";
+var SOUTH_QUAYS = "South Quays";
+var SOUTH_WIMBLEDON = "South Wimbledon";
+var SOUTH_WOODFORD = "South Woodford";
+var SOUTHFIELDS = "Southfields";
+var SOUTHGATE = "Southgate";
+var SOUTHWARK = "Southwark";
+var STANMORE = "Stanmore";
+var ST_JAMESS_PARK = "St. James's Park";
+var ST_JOHNS_WOOD = "St. John's Wood";
+var ST_PAULS = "St. Paul's";
+var STAMFORD_BROOK = "Stamford Brook";
+var STEPNEY_GREEN = "Stepney Green";
+var STOCKWELL = "Stockwell";
+var STONEBRIDGE_PARK = "Stonebridge Park";
+var STRATFORD = "Stratford";
+var SUDBURY_HILL = "Sudbury Hill";
+var SUDBURY_TOWN = "Sudbury Town";
+var SURREY_QUAYS = "Surrey Quays";
+var SWISS_COTTAGE = "Swiss Cottage";
+var TEMPLE = "Temple";
+var THEYDON_BOIS = "Theydon Bois";
+var TOOTING_BEC = "Tooting Bec";
+var TOOTING_BROADWAY = "Tooting Broadway";
+var TOTTENHAM_COURT_ROAD = "Tottenham Court Road";
+var TOTTENHAM_HALE = "Tottenham Hale";
+var TOTTERIDGE_AND_WHETSTONE = "Totteridge & Whetstone";
+var TOWER_HILL = "Tower Hill";
+var TUFNELL_PARK = "Tufnell Park";
+var TURNHAM_GREEN = "Turnham Green";
+var TURNPIKE_LANE = "Turnpike Lane";
+var UPMINSTER = "Upminster";
+var UPMINSTER_BRIDGE = "Upminster Bridge";
+var UPNEY = "Upney";
+var UPTON_PARK = "Upton Park";
+var UXBRIDGE = "Uxbridge";
+var VAUXHALL = "Vauxhall";
+var VICTORIA = "Victoria";
+var WALTHAMSTOW_CENTRAL = "Walthamstow Central";
+var WANSTEAD = "Wanstead";
+var WAPPING = "Wapping";
+var WARREN_STREET = "Warren Street";
+var WARWICK_AVENUE = "Warwick Avenue";
+var WATERLOO = "Waterloo";
+var WATFORD = "Watford";
+var WEMBLEY_CENTRAL = "Wembley Central";
+var WEMBLEY_PARK = "Wembley Park";
+var WEST_ACTON = "West Acton";
+var WEST_BROMPTON = "West Brompton";
+var WEST_FINCHLEY = "West Finchley";
+var WEST_HAM = "West Ham";
+var WEST_HAMPSTEAD = "West Hampstead";
+var WEST_HARROW = "West Harrow";
+var WEST_INDIA_QUAY = "West India Quay";
+var WEST_KENSINGTON = "West Kensington";
+var WEST_RUISLIP = "West Ruislip";
+var WESTBOURNE_PARK = "Westbourne Park";
+var WESTMINSTER = "Westminster";
+var WHITE_CITY = "White City";
+var WHITECHAPEL = "Whitechapel";
+var WILLESDEN_GREEN = "Willesden Green";
+var WILLESDEN_JUNCTION_1 = "Willesden Junction (1)";
+var WILLESDEN_JUNCTION_2 = "Willesden Junction (2)";
+var WIMBLEDON = "Wimbledon";
+var WIMBLEDON_PARK = "Wimbledon Park";
+var WOOD_LANE = "Wood Lane";
+var WOOD_GREEN = "Wood Green";
+var WOODFORD = "Woodford";
+var WOODSIDE_PARK = "Woodside Park";
+
+var MUDCHUTE = "Mudchute";
+var ISLAND_GARDENS = "Island Gardens";
+var CUTTY_SARK = "Cutty Sark for Maritime Greenwich";
+var GREENWICH = "Greenwich";
+var DEPTFORD_BRIDGE = "Deptford Bridge";
+var ELVERSON_ROAD = "Elverson Road";
+var LEWISHAM = "Lewisham";
+var TOWER_GATEWAY = "Tower Gateway";
+var LIMEHOUSE = "Limehouse";
+var WESTFERRY = "Westferry";
+var BLACKWALL = "Blackwall";
+var EAST_INDIA = "East India";
+var ROYAL_VICTORIA = "Royal Victoria";
+var EXCEL = "Custom House for ExCeL";
+var PRINCE_REGENT = "Price Regent";
+var ROYAL_ALBERT = "Royal Albert";
+var BECKTON_PARK = "Beckton Park";
+var CYPRUS = "Cyprus";
+var GALLIONS_REACH = "Gallions Reach";
+var BECKTON = "Beckton";
+var WEST_SILVERTOWN = "West Silvertown";
+var PONTOON_DOCK = "Pontoon Dock";
+var LONDON_CITY_AIRPORT = "London City Airport";
+var KING_GEORGE_V = "King George V";
+var WOOLWICH_ARSENAL = "Woolwich Arsenal";
+var SOUTH_HAMPSTEAD = "South Hampstead";
+var KILBURN_HIGH_ROAD = "Kilburn High Road";
+var HEADSTONE_LANE = "Headstone Lane";
+var HATCH_END = "Hatch End";
+var CARPENDERS_PARK = "Carpenders Park";
+var BUSHEY = "Bushey";
+var WATFORD_HIGH_STREET = "Watford High Street";
+var WATFORD_JUNCTION = "Watford Junction";
+var CRYSTAL_PALACE = "Crystal Palace";
+var BROCKLEY = "Brockley";
+var HONOR_OAK_PARK = "Honor Oak Park";
+var FOREST_HILL = "Forest Hill";
+var SYDENHAM = "Sydenham";
+var PENGE_WEST = "Penge West";
+var ANERLEY = "Anerley";
+var NORWOOD_JUNCTION = "Norwood Junction";
+var WEST_CROYDON = "West Croydon";
+var SHOREDITCH_HIGH_STREET = "Shoreditch High Street";
+var HOXTON = "Hoxton";
+var HAGGERSTON = "Haggerston";
+var DALSTON_JUNCTION = "Dalston Junction";
+var CALEDONIAN_ROAD_AND_BARNSBURY = "Caledonian Road & Barnsbury";
+var FINCHLEY_ROAD_AND_FROGNAL = "Finchley Road & Frognal";
+var IMPERIAL_WHARF = "Imperial Wharf";
+var CLAPHAM_JUNCTION = "Clapham Junction";
+var WOODGRANGE_PARK = "Woodgrange Park";
+var WANSTEAD_PARK = "Wanstead Park";
+var LEYTONSTONE_HIGH_ROAD = "Leytonstone High Road";
+var LEYTON_MIDLAND_ROAD = "Leyton Midland Road";
+var WALTHAMSTOW_QUEENS_ROAD = "Walthamstow Queen's Road";
+var SOUTH_TOTTENHAM = "South Tottenham";
+var HARRINGAY_GREEN_LANES = "Harringay Green Lanes";
+var CROUCH_HILL = "Crouch Hill";
+var UPPER_HOLLOWAY = "Upper Holloway";
+
 var lines = {};
 var stations = {};
+var stationLabels = {};
 var connections = {};
 
 function addLines() {
@@ -912,7 +1293,7 @@ function addConnections() {
     joinStation(LINE_NOTHERN, MOORGATE, OLD_STREET);
     joinStation(LINE_NOTHERN, OLD_STREET, ANGEL, "1483.01 644.15 C 1483.01 638.18 1480.59 632.78 1476.68 628.87 C 1472.77 624.95 1467.35 622.54 1461.39 622.54");
     joinStation(LINE_NOTHERN, ANGEL, KINGS_CROSS);
-    joinStation(LINE_NOTHERN, KINGS_CROSS, EUSTON_2, "1299.77 622.54 C 1288.02 622.54 1278.48 613.01 1278.48 601.25");
+    joinStation(LINE_NOTHERN, KINGS_CROSS, EUSTON_2, "1299.77 622.54 C 1288.02 622.54 1280 614 1280 614");
     joinStation(LINE_NOTHERN, EUSTON_2, CAMDEN_TOWN_2, "1278.48 535.2 C 1278.48 530.33 1275.67 523.53 1272.22 520.09 L 1271.46 519.32 C 1268.02 515.88 1265.2 509.07 1265.2 504.21");
     joinStation(LINE_NOTHERN, CAMDEN_TOWN_2, KENTISH_TOWN, "1265.2 485.88 C 1265.2 481 1268.02 474.2 1271.46 470.76 L 1334.7 407.51 C 1338.15 404.07 1340.97 397.27 1340.97 392.4");
     joinStation(LINE_NOTHERN, KENTISH_TOWN, TUFNELL_PARK);
@@ -1151,11 +1532,28 @@ function addLine(name, color) {
 	lines[id] = new Line(name, color);
 }
 
-function addStation(lineName, stationName, x, y) {
+function addStation(lineName, stationName, x, y, gravity) {
 	var id = getStationId(lineName, stationName);
 
     var line = lines[getLineId(lineName)];
 	stations[id] = new Station(stationName, line, x, y);
+
+    _addStationLabel(stationName, x, y, gravity);
+}
+
+function _addStationLabel(stationName, x, y, gravity) {
+    gravity = gravity || "east";
+
+    var id = getJustStationId(stationName);
+
+    if (id in stationLabels) {
+        var label = stationLabels[id];
+        label.x = (label.x + x) / 2;
+        label.y = (label.y + y) / 2;
+    }
+    else {
+        stationLabels[id] = new Label(stationName, gravity, x, y);
+    }
 }
 
 function joinStation(lineName, stationNameA, stationNameB, joinSvg) {
@@ -1166,14 +1564,7 @@ function joinStation(lineName, stationNameA, stationNameB, joinSvg) {
 	}
 	
 	var stationA = stations[getStationId(lineName, stationNameA)];
-    if (stationA === undefined) {
-        throw "stationA undefined: " + stationNameA;
-    }
-
 	var stationB = stations[getStationId(lineName, stationNameB)];
-    if (stationB === undefined) {
-        throw "stationB undefined: " + stationNameB;
-    }
 
 	connections[id].push(new Connection(stationA, stationB, joinSvg));
 }
@@ -1189,11 +1580,6 @@ function getJustStationId(stationName) {
 function getStationId(lineName, stationName) {
 	return getLineId(lineName) + '-' + getJustStationId(stationName);
 }
-
-// Strip out non alphanumeric characters.
-String.prototype.toAlphanumeric = function() {
-	return this.replace(/\W/g, '');
-};
 
 function Line(name, colour) {
 	this.name = name;
@@ -1214,10 +1600,6 @@ function Station(name, line, x, y) {
         return getStationId(line.name, name);
     };
 
-    this.getName = function() {
-        return name;
-    }
-
     this.getCoords = function() {
         return this.x + " " + this.y;
     };
@@ -1228,3 +1610,46 @@ function Connection(stationA, stationB, joinSvg) {
 	this.stationB = stationB;
 	this.joinSvg = joinSvg;
 }
+
+function Label(text, gravity, x, y) {
+    this.text = text;
+    this.gravity = gravity;
+    this.x = x;
+    this.y = y;
+
+    this.getAnchor = function() {
+        switch (this.gravity) {
+            case "north": return "middle";
+            case "south": return "middle";
+            case "west": return "end";
+        }
+        return "start";
+    };
+
+    this.getX = function() {
+        switch(this.gravity) {
+            case "east": return this.x + 10;
+            case "west": return this.x - 10;
+        }
+        return this.x;
+    };
+
+    this.getY = function() {
+        switch (this.gravity) {
+            case "east": return this.y - 6;
+            case "north": return this.y - 12;
+            case "south": return this.y + 20;
+            case "west": return this.y + 14;
+        }
+        return this.y;
+    };
+
+    this.getId = function() {
+        return getJustStationId(this.text);
+    };
+}
+
+// Strip out non alphanumeric characters.
+String.prototype.toAlphanumeric = function() {
+    return this.replace(/\W/g, '');
+};
